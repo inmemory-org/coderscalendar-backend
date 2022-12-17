03d55483619be65e64018615e97818fbb41ea6b9
bhaskarbhakat <bhaskarbhakat40@gmail.com>
2022-12-18 03:53:58 +0530

    check codeforces user profile completed

--- controllers/userValidation.js
+++ controllers/userValidation.js
@@ -1,13 +1,30 @@
 import "dotenv/config";
+import fetch from "node-fetch";
 
-const validateUser = (req, res) => {
+const validateUser = async (req, res) => {
   try {
-    //TO_DO
-    res.send(`valdidateUser Controller Called with ${req.username}`);
+    const response = await fetch(`https://codeforces.com/api/user.info?handles=${req.username}`, {
+      method: 'GET',
+      headers: {
+          'Accept': 'application/json',
+      },
+  })
+
+  const responseJSON = await response.json();
+  const userProfileStatus = responseJSON.status;
+  const userProfileResult = responseJSON.result[0];
+  if(userProfileStatus === 'OK')
+    res.send(`Hi ${userProfileResult.rank}`);
+  else
+    res.send(responseJSON.comment);
+
+
   } catch (error) {
     console.log(error);
   }
 };
+
+
 
 const populateRequestWithUsername = (req, res, next, username) => {
   req.username = username;
